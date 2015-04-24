cf6be8a804c80bbc24aa397173231333c746cdbb
Nick Twigg <nick@nick-web.co.uk>
2015-04-24 12:47:02 +0100

    clean up main no dis

--- nodisp.py
+++ nodisp.py
@@ -8,7 +8,7 @@
 # 3) Hook up GPS to Windows to check baud speed and transmission rates etc
 
 
- 
+
 import pygame
 import sys
 import os
@@ -24,6 +24,7 @@ from smbus import SMBus
 from ctypes import c_short
 from LSM9DS0 import *
 import threading
+from pidashinc import *
 
 
 GPIO.setmode(GPIO.BOARD)  # Use board pin numbering
@@ -38,121 +39,7 @@ gpsd = None #seting the global variable
 
 os.system('clear') #clear the terminal (optional)
 
-RAD_TO_DEG = 57.29578
-M_PI = 3.14159265358979323846
-# [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-G_GAIN = 0.070
-# Loop period = 41ms.   This needs to match the time it takes each loop to run
-LP = 0.040
-AA = 0.90      # Complementary filter constant
 
-
-def writeACC(register, value):
-    bus.write_byte_data(ACC_ADDRESS, register, value)
-    return -1
-
-
-def writeMAG(register, value):
-    bus.write_byte_data(MAG_ADDRESS, register, value)
-    return -1
-
-
-def writeGRY(register, value):
-    bus.write_byte_data(GYR_ADDRESS, register, value)
-    return -1
-
-
-def readACCx():
-    acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
-    acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
-    acc_combined = (acc_l | acc_h << 8)
-
-    return acc_combined if acc_combined < 32768 else acc_combined - 65536
-
-
-def readACCy():
-    acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
-    acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
-    acc_combined = (acc_l | acc_h << 8)
-
-    return acc_combined if acc_combined < 32768 else acc_combined - 65536
-
-
-def readACCz():
-    acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
-    acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
-    acc_combined = (acc_l | acc_h << 8)
-
-    return acc_combined if acc_combined < 32768 else acc_combined - 65536
-
-
-def readMAGx():
-    mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_X_L_M)
-    mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_X_H_M)
-    mag_combined = (mag_l | mag_h << 8)
-
-    return mag_combined if mag_combined < 32768 else mag_combined - 65536
-
-
-def readMAGy():
-    mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Y_L_M)
-    mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Y_H_M)
-    mag_combined = (mag_l | mag_h << 8)
-
-    return mag_combined if mag_combined < 32768 else mag_combined - 65536
-
-
-def readMAGz():
-    mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Z_L_M)
-    mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Z_H_M)
-    mag_combined = (mag_l | mag_h << 8)
-
-    return mag_combined if mag_combined < 32768 else mag_combined - 65536
-
-
-def readGYRx():
-    gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_X_L_G)
-    gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_X_H_G)
-    gyr_combined = (gyr_l | gyr_h << 8)
-
-    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
-
-
-def readGYRy():
-    gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Y_L_G)
-    gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Y_H_G)
-    gyr_combined = (gyr_l | gyr_h << 8)
-
-    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
-
-
-def readGYRz():
-    gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Z_L_G)
-    gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Z_H_G)
-    gyr_combined = (gyr_l | gyr_h << 8)
-
-    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
-
-
-# initialise the accelerometer
-# z,y,x axis enabled, continuos update,  100Hz data rate
-writeACC(CTRL_REG1_XM, 0b01100111)
-writeACC(CTRL_REG2_XM, 0b00100000)  # +/- 16G full scale
-
-# initialise the magnetometer
-writeMAG(CTRL_REG5_XM, 0b11110000)  # Temp enable, M data rate = 50Hz
-writeMAG(CTRL_REG6_XM, 0b01100000)  # +/-12gauss
-writeMAG(CTRL_REG7_XM, 0b00000000)  # Continuous-conversion mode
-
-# initialise the gyroscope
-writeGRY(CTRL_REG1_G, 0b00001111)  # Normal power mode, all axes enabled
-writeGRY(CTRL_REG4_G, 0b00110000)  # Continuos update, 2000 dps full scale
-
-gyroXangle = 0.0
-gyroYangle = 0.0
-gyroZangle = 0.0
-CFangleX = 0.0
-CFangleY = 0.0
 
 os.environ["SDL_FBDEV"] = "/dev/fb1"
 
